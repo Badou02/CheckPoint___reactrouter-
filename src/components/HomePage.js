ca2c76d6897c15f94238c92d2ac219d9d77d44d9
Badou02 <142432832+Badou02@users.noreply.github.com>
2024-05-06 00:33:27 +0000

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,27 +1,14 @@
 import React from 'react';
- import allfilms from '../allfilms.json';// Chemin relatif depuis les fichiers de composants
-import { Link } from 'react-router-dom';
- import FilmCard from './FilmCard';
- 
- const HomePage = () => {
-   return (
-     <div>
-      <h1>Accueil</h1>
-      <div className="film-list">
-        {allfilms.map(film => (
-          <Link to={`/details/${film.id}`} key={film.id}>
-             <FilmCard
-               title={film.title}
-               description={film.description}
-              note={film.note}
-              image={film.image}
-               trailerLink={film.trailerLink}
-             />
-           </Link >
-        ))}
-       </div>
-     </div>
-   );
- }
+import ListeDeFilms from './Listedefilms';
+import Navbar  from './Navbar';
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar/> {/* appel du fichier Navbar */}
+      <ListeDeFilms /> {/* appel du fichier */}
+    </div>
+  );
+}
 
- export default HomePage;
+export default HomePage;
+
