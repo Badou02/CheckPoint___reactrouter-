7f1d51d5876b94e8dcd1e3f1f05047bdbfcee6e0
Badou02 <142432832+Badou02@users.noreply.github.com>
2024-05-06 00:31:54 +0000

    Update FilmCard.js

--- src/components/FilmCard.js
+++ src/components/FilmCard.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
-const FilmCard = ({ title, note, description, image,trailerLink }) => {
+
+ const FilmCard = ({film}) => { // initialisation du composant 
   return (
-   <div>
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={image} alt={title} />
-      <Card.Body>
-        <Card.Title>{title }</Card.Title>
-        <Card.Text>{description}</Card.Text>
-        <Card.Text>Note:{note}</Card.Text>
-        <Card.Text>{trailerLink}</Card.Text>
+   <div>{/* syntaxe jsx avec un div qui rend une carte  */}
+   <Card style={{ width: '18rem' }}>  
+    <Link to={`/details/${film.id}`}> {/*  Utilisation du composant Link pour créer un lien vers les détails du film, en utilisant l'ID du film : */}
+      <Card.Img variant="top" src={film.image} alt={film.title} />    {/*  ajout d'une image à la carte       */}
+    </Link >
+      <Card.Body>  {/*  ajout du corps de la carte contenant les elements à afficher  */}
+        <Card.Title>{film.title }</Card.Title>
+        <Card.Text>{film.description}</Card.Text>
+        <Card.Text>Note:{film.note}</Card.Text>
+        <Card.Text>{film.trailerLink}</Card.Text>
       </Card.Body>
     </Card>
    </div> 
