9fa263bd3bc3dce81db8d3825b10d67d7ebfde4b
Badou02 <142432832+Badou02@users.noreply.github.com>
2024-05-06 00:34:23 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+function Navbar() {
+    
 
-function Navbar(){
     return(
         
             <nav className="nav">
-           <Link to="/" className="nav-link"> Accueil</Link> 
-           <Link to="/Homepage" className="nav-link"> HomePage</Link> 
+           <Link to="/" className="nav-link"> Accueil</Link>  
            <Link to="/Listedefilms" className="nav-link"> Listedefilms </Link>  
+           <Link to="/NewFilmForm" className="nav-link"> ADD New film </Link>
+          
           </nav>
         
     )
