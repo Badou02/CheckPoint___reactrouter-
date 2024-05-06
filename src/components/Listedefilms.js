68af539e031f28a7d9aa9b40a408e798bcfaabb1
Badou02 <142432832+Badou02@users.noreply.github.com>
2024-05-06 00:33:55 +0000

    Update Listedefilms.js

--- src/components/Listedefilms.js
+++ src/components/Listedefilms.js
@@ -1,21 +1,49 @@
-import allfilms from '../allfilms.json'
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom'; // Importer Link et useLocation depuis react-router-dom
 import FilmCard from './FilmCard';
+import allfilms from '../allfilms.json';
+import '../App.css';
 
-const ListeDeFilms = ({ films }) => {
-  return (
-    <div className="film-list">
-      {allfilms.map((film, index) => (
-        <FilmCard
-          key={index}
-          title={film.title} 
-          description={film.description} 
-          image={film.image} 
-          note={film.note} 
-        />
-      ))}
-    </div>
-  );
+const ListeDeFilms = () => {
+    const [filterCriteria, setFilterCriteria] = useState("");
+    const [ratingCriteria, setRatingCriteria] = useState("");
+    const location = useLocation(); // Utiliser useLocation pour obtenir l'URL actuelle
+
+    return (
+        <div className="film-list">
+           
+<div className="filter-inputs">
+            <input
+                type="text"
+                placeholder="Rechercher par titre"
+                value={filterCriteria}
+                onChange={(e) => setFilterCriteria(e.target.value)}
+            />
+            <input
+                type="text"
+                placeholder="Filtrer par note"
+                value={ratingCriteria}
+                onChange={(e) => setRatingCriteria(e.target.value)}
+            />
+             </div>
+             <div className="film-cards">
+            {allfilms.filter(film => {
+                const titleMatch = film.title.toLowerCase().includes(filterCriteria.toLowerCase());
+                const ratingMatch = film.note.toString().includes(ratingCriteria);
+                return titleMatch && ratingMatch;
+            }).map((film, index) => (
+                <FilmCard
+                    key={index}
+                    film={film}
+                />
+            ))}
+        </div>
+         {/* Utiliser une logique conditionnelle pour rendre le bouton uniquement si l'URL n'est pas la page principale */}
+         {location.pathname !== "/" && (
+                <button onClick={() => window.location.href = "/"}>Retour à la page principale</button>
+            )}
+           </div> 
+    );
 };
 
 export default ListeDeFilms;
